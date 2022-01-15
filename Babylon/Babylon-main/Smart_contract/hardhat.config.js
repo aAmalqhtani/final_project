a6af5b61966465d5043c471cdb7d11ab7ac21f68
amal alqhtani <92275242+aAmalqhtani@users.noreply.github.com>
2022-01-16 01:59:01 +0300

    Add files via upload

---
+++ Babylon/Babylon-main/Smart_contract/hardhat.config.js
@@ -0,0 +1,19 @@
+//https://eth-ropsten.alchemyapi.io/v2/nSKJ0IOjwNEmmSa_zdIpmCuv1tONmIsh
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports ={
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+
+      url:"https://eth-ropsten.alchemyapi.io/v2/nSKJ0IOjwNEmmSa_zdIpmCuv1tONmIsh",
+      accounts:['3f873c40f83f4ce52f2c6f968baf426f0b56769d00e361de7ec46bef0b27e442']
+    }
+
+
+  }
+
+}
+
+//0x30B68Cda7bBbC0aaCb2e59A42F862DfC9a4Ab009
---
+++ Babylon/Babylon-main/client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x30B68Cda7bBbC0aaCb2e59A42F862DfC9a4Ab009";
